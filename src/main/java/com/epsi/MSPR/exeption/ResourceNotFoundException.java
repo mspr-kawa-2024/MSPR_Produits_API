979b15c1281550c5aee78d3e6c53b15938310ac1
fathallah <fathallah.mohamed.a@gmail.com>
2024-06-15 20:58:01 +0200

    Suppression de tout le contenu de la branche main

--- src/main/java/com/epsi/MSPR/exeption/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.epsi.MSPR.exeption;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
